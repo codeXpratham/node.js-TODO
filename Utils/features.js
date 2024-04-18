d19332769fd7fb3fff64952b3cbdaa015009443e
codeXpratham <85034123+codeXpratham@users.noreply.github.com>
2024-04-19 01:32:10 +0530

    Update features.js
    uyf

--- Utils/features.js
+++ Utils/features.js
@@ -8,6 +8,7 @@ export const sendCookies = (user , res, message , statusCode = 200) => {
         httpOnly: true, 
         maxage : 1000 * 60 * 15, 
         /*sameSite : process.env.NODE_ENV !== 'Development' ? "lax" : "none",*/
+        sameSite : "none",
         secure : process.env.NODE_ENV !== 'Development' ? false :true, 
 
      }).json({
